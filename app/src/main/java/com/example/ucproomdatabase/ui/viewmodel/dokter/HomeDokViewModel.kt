91965735fa3e2b832fbd121216b3f9981ea6219a
RezaAzharii <rezaazhari867@gmail.com>
2024-12-24 17:28:13 +0700

    membuat HomeDokViewModel untuk mengatur stateUI dan data Dokter, handle loading dan error

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/dokter/HomeDokViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/dokter/HomeDokViewModel.kt
@@ -1,7 +1,50 @@
 package com.example.ucproomdatabase.ui.viewmodel.dokter
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucproomdatabase.data.entity.Dokter
+import com.example.ucproomdatabase.repository.RepositoryDokter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
+class HomeDokViewModel(
+    private val repositoryDokter: RepositoryDokter
+) : ViewModel(){
+    val homeDokUiState: StateFlow<HomeDokUiState> = repositoryDokter.getAllDok()
+        .filterNotNull()
+        .map {
+            HomeDokUiState(
+                listDok = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeDokUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeDokUiState(
+                    isLoading = false,
+                    isError = false,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeDokUiState(
+                isLoading = true
+            )
+        )
+}
 
 data class HomeDokUiState(
     val listDok: List<Dokter> = listOf(),
