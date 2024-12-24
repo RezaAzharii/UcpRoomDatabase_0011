7a6435ac3c30452fbfacc9b03a9e4131c170f269
RezaAzharii <rezaazhari867@gmail.com>
2024-12-24 19:26:04 +0700

    Memaggil PengelolaHalaman

--- app/src/main/java/com/example/ucproomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/ucproomdatabase/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ucproomdatabase.ui.navigation.PengelolaHalaman
 import com.example.ucproomdatabase.ui.theme.UCPRoomDatabaseTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +21,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UCPRoomDatabaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier
+                    .fillMaxSize()
+                ) { innerPadding ->
+                    PengelolaHalaman(
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
